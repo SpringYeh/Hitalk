c173a354a5630d324ba8916b7d2d6ac0a79d54c6
cinzano <springyeh@163.com>
2020-06-26 10:16:51 +0800

    2

--- src/Hitalk.js
+++ src/Hitalk.js
@@ -9,6 +9,8 @@ import md5 from 'blueimp-md5'
 import marked from 'marked'
 import detect from './detect'
 import './Hitalk.scss'
+import { version } from '../package'
+import { HtmlUtil, Event, check, getLink, dateFormat, timeAgo } from './utils'
 
 const gravatar = {
   cdn: 'https://gravatar.loli.net/avatar/',
@@ -50,7 +52,7 @@ let subfix = ''
 class Hitalk {
   constructor(option) {
     this.md5 = md5
-    this.version = '1.2.0'
+    this.version = version
     getIp();
 
     const av = option.av || AV
@@ -285,89 +287,7 @@ class Hitalk {
       }
     }
 
-    let query = () => {
-      this.loading.show()
-      let cq = this.commonQuery()
-      cq.limit(this.pageSize)
-        .skip(this.page * this.pageSize)
-        .find()
-        .then(rets => {
-          let len = rets.length
 
-          if (len == 0) {
-            addSmilies() // 填充表情
-          }
-
-          if (len) {
-            this.el.querySelector('.vlist').innerHTML = ''
-            for (let i = 0; i < len; i++) {
-              insertDom(rets[i], !0)
-            }
-            let _count = this.el.querySelector('.num')
-            if (!_count) {
-              addSmilies() // 填充表情
-
-              cq.count().then(len => {
-                const _pageCount = parseInt(len / this.pageSize) + 1
-                this.el.querySelector('.count').innerHTML = `<i class="iconfont icon-mark"></i> Comments | <span class="noticom"><a><span class="num">${len}</span> 条评论</a></span>`
-
-                const _pageDom = (_class, _text) => `<span class="${_class} page-numbers">${_text}</span>`
-                let vpageDom = _pageDom('prev dn', '<i class="fa fa-chevron-left"></i>')
-                if (_pageCount !== 1) {
-                  for (let index = 1; index <= _pageCount; index++) {
-                    vpageDom += _pageDom(`numbers ${index == 1 ? 'current' : ''}`, index)
-                  }
-                }
-
-                vpageDom += _pageDom('next dn', '<i class="fa fa-chevron-right"></i>')
-                this.el.querySelector('.vpage').innerHTML = vpageDom
-                pageHandle(len)
-
-                Event.on('click', this.el.querySelector('.vpage'), e => {
-                  const inc = v => e.target.className.split(' ').includes(v)
-                  if (inc('current') || inc('vpage')) {
-                    return
-                  }
-                  if (inc('numbers')) {
-                    this.page = Number(e.target.innerText) - 1
-                  } else if (inc('prev')) {
-                    this.page--
-                  } else if (inc('next')) {
-                    this.page++
-                  }
-                  query()
-                })
-              })
-            } else {
-              pageHandle(_count.innerText)
-            }
-          }
-          this.loading.hide()
-        })
-        .catch(ex => {
-          this.loading.hide()
-          this.throw(ex)
-        })
-    }
-    query()
-
-    let pageHandle = _count => {
-      if (this.el.querySelector('.vpage .numbers.current')) {
-        this.el.querySelector('.vpage .numbers.current').classList.remove('current')
-      }
-      this.el.querySelectorAll('.vpage .numbers')[this.page].classList.add('current')
-
-      // 隐藏prev、next按钮
-      // const domClass = {
-      //   0: '.prev',
-      //   [parseInt(_count / this.pageSize, 10)]: '.next'
-      // }
-      // // Object.values(domClass).map(e => this.el.querySelector(`.vpage ${e}`).classList.remove('dn'))
-      // // if (domClass[this.page]) {
-      // //   this.el.querySelector(`.vpage ${domClass[this.page]}`).classList.add('dn')
-      // //   return
-      // // }
-    }
 
     let insertDom = (ret, mt) => {
       let _vcard = document.createElement('li')
@@ -471,6 +391,90 @@ class Hitalk {
           smilies.classList.remove('smilies-open')
         }
       })
+    }
+
+    let query = () => {
+      this.loading.show()
+      let cq = this.commonQuery()
+      cq.limit(this.pageSize)
+        .skip(this.page * this.pageSize)
+        .find()
+        .then(rets => {
+          let len = rets.length
+
+          if (len == 0) {
+            addSmilies() // 填充表情
+          }
+
+          if (len) {
+            this.el.querySelector('.vlist').innerHTML = ''
+            for (let i = 0; i < len; i++) {
+              insertDom(rets[i], !0)
+            }
+            let _count = this.el.querySelector('.num')
+            if (!_count) {
+              addSmilies() // 填充表情
+
+              cq.count().then(len => {
+                const _pageCount = parseInt(len / this.pageSize) + 1
+                this.el.querySelector('.count').innerHTML = `<i class="iconfont icon-mark"></i> Comments | <span class="noticom"><a><span class="num">${len}</span> 条评论</a></span>`
+
+                const _pageDom = (_class, _text) => `<span class="${_class} page-numbers">${_text}</span>`
+                let vpageDom = _pageDom('prev dn', '<i class="fa fa-chevron-left"></i>')
+                if (_pageCount !== 1) {
+                  for (let index = 1; index <= _pageCount; index++) {
+                    vpageDom += _pageDom(`numbers ${index == 1 ? 'current' : ''}`, index)
+                  }
+                }
+
+                vpageDom += _pageDom('next dn', '<i class="fa fa-chevron-right"></i>')
+                this.el.querySelector('.vpage').innerHTML = vpageDom
+                pageHandle(len)
+
+                Event.on('click', this.el.querySelector('.vpage'), e => {
+                  const inc = v => e.target.className.split(' ').includes(v)
+                  if (inc('current') || inc('vpage')) {
+                    return
+                  }
+                  if (inc('numbers')) {
+                    this.page = Number(e.target.innerText) - 1
+                  } else if (inc('prev')) {
+                    this.page--
+                  } else if (inc('next')) {
+                    this.page++
+                  }
+                  query()
+                })
+              })
+            } else {
+              pageHandle(_count.innerText)
+            }
+          }
+          this.loading.hide()
+        })
+        .catch(ex => {
+          this.loading.hide()
+          this.throw(ex)
+        })
+    }
+    query()
+
+    let pageHandle = _count => {
+      if (this.el.querySelector('.vpage .numbers.current')) {
+        this.el.querySelector('.vpage .numbers.current').classList.remove('current')
+      }
+      this.el.querySelectorAll('.vpage .numbers')[this.page].classList.add('current')
+
+      // 隐藏prev、next按钮
+      // const domClass = {
+      //   0: '.prev',
+      //   [parseInt(_count / this.pageSize, 10)]: '.next'
+      // }
+      // // Object.values(domClass).map(e => this.el.querySelector(`.vpage ${e}`).classList.remove('dn'))
+      // // if (domClass[this.page]) {
+      // //   this.el.querySelector(`.vpage ${domClass[this.page]}`).classList.add('dn')
+      // //   return
+      // // }
     }
 
     let mapping = {
@@ -714,125 +718,6 @@ class Hitalk {
   }
 }
 
-const Event = {
-  on(type, el, handler, capture) {
-    if (el.addEventListener) el.addEventListener(type, handler, capture || false)
-    else if (el.attachEvent) el.attachEvent(`on${type}`, handler)
-    else el[`on${type}`] = handler
-  },
-  off(type, el, handler, capture) {
-    if (el.removeEventListener) el.removeEventListener(type, handler, capture || false)
-    else if (el.detachEvent) el.detachEvent(`on${type}`, handler)
-    else el[`on${type}`] = null
-  }
-}
-
-const getLink = target => {
-  return target.link || (target.mail && `mailto:${target.mail}`) || 'javascript:void(0);'
-}
-
-const check = {
-  mail(m) {
-    return {
-      k: /[\w-\.]+@([\w-]+\.)+[a-z]{2,3}/.test(m),
-      v: m
-    }
-  },
-  link(l) {
-    l = l.length > 0 && (/^(http|https)/.test(l) ? l : `http://${l}`)
-    return {
-      k: /(http|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?/.test(l),
-      v: l
-    }
-  }
-}
-
-const HtmlUtil = {
-  /**
-   * HTML转码
-   * @param {String} str
-   * @return {String} result
-   */
-  encode(str) {
-    return !!str ?
-      str
-        .replace(/&/g, '&amp;')
-        .replace(/</g, '&lt;')
-        .replace(/>/g, '&gt;')
-        .replace(/ /g, '&nbsp;')
-        .replace(/\'/g, '&#39;')
-        .replace(/\"/g, '&quot;') :
-      ''
-  },
-  /**
-   * HTML解码
-   * @param {String} str
-   * @return {String} result
-   */
-  decode(str) {
-    return !!str ?
-      str
-        .replace(/&amp;/g, '&')
-        .replace(/&lt;/g, '<')
-        .replace(/&gt;/g, '>')
-        .replace(/&nbsp;/g, ' ')
-        .replace(/&#39;/g, "'")
-        .replace(/&quot;/g, '"') :
-      ''
-  }
-}
-
-const dateFormat = date => {
-  const vDay = padWithZeros(date.getDate(), 2)
-  const vMonth = padWithZeros(date.getMonth() + 1, 2)
-  const vYear = padWithZeros(date.getFullYear(), 2)
-  return `${vYear}-${vMonth}-${vDay}`
-}
-
-const timeAgo = date => {
-  try {
-    const oldTime = date.getTime()
-    const currTime = new Date().getTime()
-    const diffValue = currTime - oldTime
-
-    const days = Math.floor(diffValue / (24 * 3600 * 1000))
-    if (days === 0) {
-      //计算相差小时数
-      const leave1 = diffValue % (24 * 3600 * 1000) //计算天数后剩余的毫秒数
-      const hours = Math.floor(leave1 / (3600 * 1000))
-      if (hours === 0) {
-        //计算相差分钟数
-        const leave2 = leave1 % (3600 * 1000) //计算小时数后剩余的毫秒数
-        const minutes = Math.floor(leave2 / (60 * 1000))
-        if (minutes === 0) {
-          //计算相差秒数
-          const leave3 = leave2 % (60 * 1000) //计算分钟数后剩余的毫秒数
-          const seconds = Math.round(leave3 / 1000)
-          return seconds + ' 秒前'
-        }
-        return minutes + ' 分钟前'
-      }
-      return hours + ' 小时前'
-    }
-    if (days < 0) return '刚刚'
-
-    if (days < 8) {
-      return days + ' 天前'
-    } else {
-      return dateFormat(date)
-    }
-  } catch (error) {
-    console.log(error)
-  }
-}
-
-const padWithZeros = (vNumber, width) => {
-  let numAsString = vNumber.toString()
-  while (numAsString.length < width) {
-    numAsString = '0' + numAsString
-  }
-  return numAsString
-}
 
 const getIp = function () {
   $.getJSON("https://api.ipify.org/?format=json",
