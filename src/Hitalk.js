39602c32f14c9f8a29f7e261aec0643464efd36d
cinzano <springyeh@163.com>
2020-06-26 10:53:15 +0800

    1

--- src/Hitalk.js
+++ src/Hitalk.js
@@ -287,7 +287,89 @@ class Hitalk {
       }
     }
 
+    let query = () => {
+      this.loading.show()
+      let cq = this.commonQuery()
+      cq.limit(this.pageSize)
+        .skip(this.page * this.pageSize)
+        .find()
+        .then(rets => {
+          let len = rets.length
 
+          if (len == 0) {
+            addSmilies() // 填充表情
+          }
+
+          if (len) {
+            this.el.querySelector('.vlist').innerHTML = ''
+            for (let i = 0; i < len; i++) {
+              insertDom(rets[i], !0)
+            }
+            let _count = this.el.querySelector('.num')
+            if (!_count) {
+              addSmilies() // 填充表情
+
+              cq.count().then(len => {
+                const _pageCount = parseInt(len / this.pageSize) + 1
+                this.el.querySelector('.count').innerHTML = `<i class="iconfont icon-mark"></i> Comments | <span class="noticom"><a><span class="num">${len}</span> 条评论</a></span>`
+
+                const _pageDom = (_class, _text) => `<span class="${_class} page-numbers">${_text}</span>`
+                let vpageDom = _pageDom('prev dn', '<i class="fa fa-chevron-left"></i>')
+                if (_pageCount !== 1) {
+                  for (let index = 1; index <= _pageCount; index++) {
+                    vpageDom += _pageDom(`numbers ${index == 1 ? 'current' : ''}`, index)
+                  }
+                }
+
+                vpageDom += _pageDom('next dn', '<i class="fa fa-chevron-right"></i>')
+                this.el.querySelector('.vpage').innerHTML = vpageDom
+                pageHandle(len)
+
+                Event.on('click', this.el.querySelector('.vpage'), e => {
+                  const inc = v => e.target.className.split(' ').includes(v)
+                  if (inc('current') || inc('vpage')) {
+                    return
+                  }
+                  if (inc('numbers')) {
+                    this.page = Number(e.target.innerText) - 1
+                  } else if (inc('prev')) {
+                    this.page--
+                  } else if (inc('next')) {
+                    this.page++
+                  }
+                  query()
+                })
+              })
+            } else {
+              pageHandle(_count.innerText)
+            }
+          }
+          this.loading.hide()
+        })
+        .catch(ex => {
+          this.loading.hide()
+          this.throw(ex)
+        })
+    }
+    query()
+
+    let pageHandle = _count => {
+      if (this.el.querySelector('.vpage .numbers.current')) {
+        this.el.querySelector('.vpage .numbers.current').classList.remove('current')
+      }
+      this.el.querySelectorAll('.vpage .numbers')[this.page].classList.add('current')
+
+      // 隐藏prev、next按钮
+      // const domClass = {
+      //   0: '.prev',
+      //   [parseInt(_count / this.pageSize, 10)]: '.next'
+      // }
+      // // Object.values(domClass).map(e => this.el.querySelector(`.vpage ${e}`).classList.remove('dn'))
+      // // if (domClass[this.page]) {
+      // //   this.el.querySelector(`.vpage ${domClass[this.page]}`).classList.add('dn')
+      // //   return
+      // // }
+    }
 
     let insertDom = (ret, mt) => {
       let _vcard = document.createElement('li')
@@ -391,90 +473,6 @@ class Hitalk {
           smilies.classList.remove('smilies-open')
         }
       })
-    }
-
-    let query = () => {
-      this.loading.show()
-      let cq = this.commonQuery()
-      cq.limit(this.pageSize)
-        .skip(this.page * this.pageSize)
-        .find()
-        .then(rets => {
-          let len = rets.length
-
-          if (len == 0) {
-            addSmilies() // 填充表情
-          }
-
-          if (len) {
-            this.el.querySelector('.vlist').innerHTML = ''
-            for (let i = 0; i < len; i++) {
-              insertDom(rets[i], !0)
-            }
-            let _count = this.el.querySelector('.num')
-            if (!_count) {
-              addSmilies() // 填充表情
-
-              cq.count().then(len => {
-                const _pageCount = parseInt(len / this.pageSize) + 1
-                this.el.querySelector('.count').innerHTML = `<i class="iconfont icon-mark"></i> Comments | <span class="noticom"><a><span class="num">${len}</span> 条评论</a></span>`
-
-                const _pageDom = (_class, _text) => `<span class="${_class} page-numbers">${_text}</span>`
-                let vpageDom = _pageDom('prev dn', '<i class="fa fa-chevron-left"></i>')
-                if (_pageCount !== 1) {
-                  for (let index = 1; index <= _pageCount; index++) {
-                    vpageDom += _pageDom(`numbers ${index == 1 ? 'current' : ''}`, index)
-                  }
-                }
-
-                vpageDom += _pageDom('next dn', '<i class="fa fa-chevron-right"></i>')
-                this.el.querySelector('.vpage').innerHTML = vpageDom
-                pageHandle(len)
-
-                Event.on('click', this.el.querySelector('.vpage'), e => {
-                  const inc = v => e.target.className.split(' ').includes(v)
-                  if (inc('current') || inc('vpage')) {
-                    return
-                  }
-                  if (inc('numbers')) {
-                    this.page = Number(e.target.innerText) - 1
-                  } else if (inc('prev')) {
-                    this.page--
-                  } else if (inc('next')) {
-                    this.page++
-                  }
-                  query()
-                })
-              })
-            } else {
-              pageHandle(_count.innerText)
-            }
-          }
-          this.loading.hide()
-        })
-        .catch(ex => {
-          this.loading.hide()
-          this.throw(ex)
-        })
-    }
-    query()
-
-    let pageHandle = _count => {
-      if (this.el.querySelector('.vpage .numbers.current')) {
-        this.el.querySelector('.vpage .numbers.current').classList.remove('current')
-      }
-      this.el.querySelectorAll('.vpage .numbers')[this.page].classList.add('current')
-
-      // 隐藏prev、next按钮
-      // const domClass = {
-      //   0: '.prev',
-      //   [parseInt(_count / this.pageSize, 10)]: '.next'
-      // }
-      // // Object.values(domClass).map(e => this.el.querySelector(`.vpage ${e}`).classList.remove('dn'))
-      // // if (domClass[this.page]) {
-      // //   this.el.querySelector(`.vpage ${domClass[this.page]}`).classList.add('dn')
-      // //   return
-      // // }
     }
 
     let mapping = {
@@ -717,6 +715,7 @@ class Hitalk {
     Event.on('click', submitBtn, submitEvt)
   }
 }
+
 
 
 const getIp = function () {
--- src/query.js
+++ src/query.js
@@ -1,78 +1,75 @@
 let query = () => {
-  this.loading.show()
-  let cq = this.commonQuery()
-  cq.limit(this.pageSize)
-    .skip(this.page * this.pageSize)
-    .find()
-    .then(rets => {
+    this.loading.show()
+    let cq = this.commonQuery()
+    cq.limit(this.pageSize)
+        .skip(this.page * this.pageSize)
+        .find()
+        .then(rets => {
+            let len = rets.length
+            if (len) {
+                this.el.querySelector('.vlist').innerHTML = ''
+                for (let i = 0; i < len; i++) {
+                    insertDom(rets[i], !0)
+                }
+                let _count = this.el.querySelector('.num')
+                if (!_count) {
+                    addSmilies() // 填充表情
 
-      addSmilies() // 填充表情
+                    cq.count().then(len => {
+                        const _pageCount = len / this.pageSize
+                        this.el.querySelector('.count').innerHTML = `评论(<span class="num">${len}</span>)`
 
-      let len = rets.length
-      if (len) {
-        this.el.querySelector('.vlist').innerHTML = ''
-        for (let i = 0; i < len; i++) {
-          insertDom(rets[i], !0)
-        }
-        let _count = this.el.querySelector('.num')
-        if (!_count) {
-          // addSmilies() // 填充表情
+                        const _pageDom = (_class, _text) => `<span class="${_class} page-numbers">${_text}</span>`
+                        let vpageDom = _pageDom('prev dn', '&lt;')
+                        for (let index = 1; index < _pageCount; index++) {
+                            vpageDom += _pageDom(`numbers ${index == 1 ? 'current' : ''}`, index)
+                        }
+                        vpageDom += _pageDom('next dn', '&gt;')
+                        this.el.querySelector('.vpage').innerHTML = vpageDom
+                        pageHandle(len)
 
-          cq.count().then(len => {
-            const _pageCount = len / this.pageSize
-            this.el.querySelector('.count').innerHTML = `评论(<span class="num">${len}</span>)`
-
-            const _pageDom = (_class, _text) => `<span class="${_class} page-numbers">${_text}</span>`
-            let vpageDom = _pageDom('prev dn', '&lt;')
-            for (let index = 1; index < _pageCount; index++) {
-              vpageDom += _pageDom(`numbers ${index == 1 ? 'current' : ''}`, index)
+                        Event.on('click', this.el.querySelector('.vpage'), e => {
+                            const inc = v => e.target.className.split(' ').includes(v)
+                            if (inc('current') || inc('vpage')) {
+                                return
+                            }
+                            if (inc('numbers')) {
+                                this.page = Number(e.target.innerText) - 1
+                            } else if (inc('prev')) {
+                                this.page--
+                            } else if (inc('next')) {
+                                this.page++
+                            }
+                            query()
+                        })
+                    })
+                } else {
+                    pageHandle(_count.innerText)
+                }
             }
-            vpageDom += _pageDom('next dn', '&gt;')
-            this.el.querySelector('.vpage').innerHTML = vpageDom
-            pageHandle(len)
-
-            Event.on('click', this.el.querySelector('.vpage'), e => {
-              const inc = v => e.target.className.split(' ').includes(v)
-              if (inc('current') || inc('vpage')) {
-                return
-              }
-              if (inc('numbers')) {
-                this.page = Number(e.target.innerText) - 1
-              } else if (inc('prev')) {
-                this.page--
-              } else if (inc('next')) {
-                this.page++
-              }
-              query()
-            })
-          })
-        } else {
-          pageHandle(_count.innerText)
-        }
-      }
-      this.loading.hide()
-    })
-    .catch(ex => {
-      this.loading.hide()
-      this.throw(ex)
-    })
+            this.loading.hide()
+        })
+        .catch(ex => {
+            this.loading.hide()
+            this.throw(ex)
+        })
 }
 query()
 
 let pageHandle = _count => {
-  if (this.el.querySelector('.vpage .numbers.current')) {
-    this.el.querySelector('.vpage .numbers.current').classList.remove('current')
-  }
-  this.el.querySelectorAll('.vpage .numbers')[this.page].classList.add('current')
+    if (this.el.querySelector('.vpage .numbers.current')) {
+        this.el.querySelector('.vpage .numbers.current').classList.remove('current')
+    }
+    this.el.querySelectorAll('.vpage .numbers')[this.page].classList.add('current')
 
-  const domClass = {
-    0: '.prev',
-    [parseInt(_count / this.pageSize, 10) - 1]: '.next'
-  }
+    const domClass = {
+        0: '.prev',
+        [parseInt(_count / this.pageSize, 10) - 1]: '.next'
+    }
 
-  Object.values(domClass).map(e => this.el.querySelector(`.vpage ${e}`).classList.remove('dn'))
-  if (domClass[this.page]) {
-    this.el.querySelector(`.vpage ${domClass[this.page]}`).classList.add('dn')
-    return
-  }
+    Object.values(domClass).map(e => this.el.querySelector(`.vpage ${e}`).classList.remove('dn'))
+    if (domClass[this.page]) {
+        this.el.querySelector(`.vpage ${domClass[this.page]}`).classList.add('dn')
+        return
+    }
 }
